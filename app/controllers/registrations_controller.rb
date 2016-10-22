e5310dacaba7acd87d5fafe324994a33a49c1e2c
lfnascimeto <nascimento.lsfo@gmail.com>
2016-10-21 23:41:02 -0200

    RegistrationController(devise) sobrescrito com token

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
-  protected
 
   def after_sign_up_path_for(resource)
     new_organizacao_path # Or :prefix_to_your_route
   end
 
   def new
+    super
     @token = params[:invite_token]
   end
   
   def create
-    @newUser = build_user(user_params)
-    @newUser.save
+    super
     @token = params[:invite_token]
     if @token
       org =  Invite.find_by_token(@token).organizacao #find the user group attached to the invite
