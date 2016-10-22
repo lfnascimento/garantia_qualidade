44e5135cda52f356bba8282aaeb61baa90453db7
lfnascimeto <nascimento.lsfo@gmail.com>
2016-10-22 21:33:45 -0200

    RegistrationController::new setting token invite

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,36 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def after_sign_up_path_for(resource)
-    new_organizacao_path # Or :prefix_to_your_route
+  def new
+    @token = params[:invite_token]
+    super
   end
 
-  def new
-    super
-    @token = params[:invite_token]
-  end
-  
   def create
-    super
-    @token = params[:invite_token]
-    if @token
-      org =  Invite.find_by_token(@token).organizacao #find the user group attached to the invite
-      @newUser.update_attribute(:organizacao, org) #add this user to the new user group as a member
+    build_resource(sign_up_params)
+    resource.organizacao = Invite.find_by_token(@token).organizacao if @token
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
     end
+  end
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    new_organizacao_path # Or :prefix_to_your_route
   end
 
 end
